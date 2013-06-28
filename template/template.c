c4d348f4b78a81798344a89a6a4c692774b5d65c
oscarmorrison <me@oscarmorrison.com>
2013-06-28 10:10:23 +1000

    initial vim commit

---
+++ template/template.c
@@ -0,0 +1,16 @@
+/******************************
+*    Title: ${FileName}               
+*   Author: OscarM
+*  Uni No#: 360324	                 
+* Language: C Language                   
+*     Date: ${date}                
+******************************/
+
+#include <stdio.h> 
+
+int main(int argc, char *argv[])
+{
+	${cursor}
+
+    return 0; 
+} 
---
+++ template/template.java
@@ -0,0 +1,14 @@
+/******************************
+*    Title: ${filename}               
+*   Author: OscarM
+*  Uni No#: 360324	                 
+* Language: Java                   
+*     Date: ${date}                
+******************************/
+public class ${filename}
+{
+    public static void main(String[] args)
+    {
+        ${cursor}
+    }
+}
